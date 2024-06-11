fe3d3532b70c0ee78bc7fe094308e39d22f7a628
Christina Vekri <christina.vek@gmail.com>
2024-06-11 17:31:56 +0200

    single note done

--- src/views/notes.jsx
+++ src/views/notes.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 
 export const Notes = () => {
-  const [newNotes, setNewNotes] = useState([]);
+  const [newNotes, setNewNotes] = useState(["note1", "note2", "note3", "note4", "note5", "note6", "note7", "note8", "note9"]);
   const [error, setError] = useState(null);
+  const [add, setAdd] = useState(false);
 
   // useEffect(async() => {
   //   try {
@@ -17,26 +18,39 @@ export const Notes = () => {
   //   }
   // }, []);
 
+  const addbtn = () => {
+    setAdd(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNewNotes([...newNotes, e.target[0].value]);
+    setAdd(false);
+  };
+
+
 
   return (
     <div>
       <h1>Notes</h1>
+      <button onClick={addbtn}>+</button>
       <div className="notes_body">
-        <div className="notes_note">
-          <p>
-            Note 1
-          </p>
-        </div>
-        <div className="notes_note">
-          <p>
-            Note 2
-          </p>
-        </div>
-        <div className="notes_note">
-          <p>
-            Note 3
-          </p>
-        </div>
+
+        {add && 
+        <div>
+          <div className="notes_note">
+          <form onSubmit={(e)=>handleSubmit(e)}>
+            <input type="text" />
+            <button type="submit">Save</button>
+          </form>
+          </div>
+        </div>}
+
+        {newNotes.map((note, index) =>
+        <div key={index} className="notes_note">
+          <p>{note}</p>
+          <button onClick={(note)=>handleDelete(note)}>Delete Note</button>
+        </div>)}
       </div>
     </div>
   );
